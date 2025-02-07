fcbe14c77583eb6de262078a168b3082bd3c1e3b
Abdulk077 <Khanabdulwadud3@gmail.com>
2025-02-07 19:54:10 +0530

    set the resolvers

---
+++ server/index.js
@@ -0,0 +1,65 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+import dotenv from "dotenv";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/middleware/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import mergedResolvers from "./resolvers/index.js";
+import mergedTypeDefs from "./typeDefs/index.js";
+import { buildContext } from "graphql-passport";
+
+import { connectDB } from "./db/connectDB.js";
+import { configurePassport } from "./passport/passport.config.js";
+dotenv.config();
+configurePassport();
+const httpServer = http.createServer(app);
+const MongoDBStore = connectMongo(session);
+
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URI,
+  collection: "sessions",
+});
+
+store.on("error", (err) => console.log(err));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false, // this option specifies whether to save the session to the store on every request
+    saveUninitialized: false, // option specifies whether to save uninitialized sessions
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true, // this option prevents the Cross-Site Scripting (XSS) attacks
+    },
+    store: store,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const server = new ApolloServer({
+  typeDefs: mergedTypeDefs,
+  resolvers: mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+
+// Ensure we wait for our server to start
+await server.start();
+app.use(
+  "/",
+  cors({
+    origin: "http://localhost",
+    credentials: true, // enable cookies
+  }),
+  express.json(), // parse JSON bodies into req.body
+  expressMiddleware(server),
+  {
+    context: async ({ req, res }) => buildContext({ req, res }),
+  }
+);
+// Set up our Express middleware to handle CORS, body parsing,
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+await connectDB();
+
+console.log(`🚀 Server ready at http://localhost:4000/graphql`);
